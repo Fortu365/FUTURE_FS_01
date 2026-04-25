5458866c1c8e54fb54a74dbc8cceb6237de59316
Fortunate <fortubelogift@gmail.com>
2026-04-25 21:39:27 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+// App.js
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter,Routes,Route,Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import HomePage from './components/HomePage';
 import Skills from './components/Skills';
@@ -11,22 +12,17 @@ import Projects from './components/Projects';
 function App() {
   return (
     <BrowserRouter>
-    
-    <div>
-<NavBar/>
-{/* <p>What if I change this to something else</p> */}
-
-<Routes>
-
-  <Route path='/' element={<HomePage/>}/>
-  <Route path='/skills' element={<Skills/>}/>
-  <Route path='/contact-me' element={<ContactMe/>}/>
-  <Route path='/projects' element={<Projects/>}/>
-</Routes>
-
-    </div>
-    
-    
+      <div className="app">
+        <NavBar />
+        <main>
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/skills' element={<Skills />} />
+            <Route path='/contact-me' element={<ContactMe />} />
+            <Route path='/projects' element={<Projects />} />
+          </Routes>
+        </main>
+      </div>
     </BrowserRouter>
   );
 }
